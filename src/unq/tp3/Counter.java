f55acc7adc7aa6cc27c182744c7d16245f71ff08
NicolasCervino <you@example.com>
2022-04-09 22:28:25 -0300

    Se agregaron test para la clase Counter

--- src/unq/tp3/Counter.java
+++ src/unq/tp3/Counter.java
@@ -6,9 +6,14 @@ public class Counter {
 	// Atributos
 	private ArrayList<Integer> numeros;
 	
-	// Constructor
+	// Constructor con parametro
 	public Counter(ArrayList<Integer> _numeros) {
 		this.setNumeros(_numeros);
+	}
+	
+	// Constructor vacio
+	public Counter() {
+		numeros= new ArrayList<Integer>();
 	}
 	
 	// Metodos
@@ -40,9 +45,32 @@ public class Counter {
 		return resultado;
 	}
 	
+	// Devuelve los numeros del array que son multiplos del numero dado por parametro
+	public ArrayList<Integer> getMultiplesOf(int _number) {
+		ArrayList<Integer> numeros = this.getNumeros();
+		ArrayList<Integer> resultado = new ArrayList<Integer>();
+		for(int i=0; i< numeros.size(); i++) {
+			if(isMultiple(_number,numeros.get(i))) {
+				resultado.add(numeros.get(i));
+			}
+		}
+		return resultado;
+	}
+	
+	// Devuelve la cantidad de numeros del array que son multiplos del numero dado por parametro
+	public int multiplesOf(int _number) {
+		return getMultiplesOf(_number).size();
+	}
+	
+	
 	// Indica si un numero es par
 	private boolean isEven(int _numero) {
 		return _numero%2 == 0;
+	}
+	
+	//Indica si 2 numeros son multiplos entre si
+	private boolean isMultiple(int num1, int num2) {
+		return num1%num2 == 0;
 	}
 	
 	//Getters y Setters
